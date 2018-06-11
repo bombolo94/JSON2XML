c0c7c2413137951abe124871842b1f19c4260282
Andrea Bombino <bomb-94@hotmail.it>
2018-06-11 14:05:31 +0200

    Unico progetto JSON2XML e XML2JSON

--- JSON2XML/src/json2xml/main/Json2Xml.java
+++ JSON2XML/src/json2xml/main/Json2Xml.java
@@ -2,7 +2,11 @@ package json2xml.main;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
 import javax.xml.parsers.*;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
@@ -18,11 +22,14 @@ import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.w3c.dom.*;
-import org.w3c.dom.Element;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import json2xml.container.Container;
+import json2xml.container.UrbanDataset;
 import json2xml.context.Context;
 import json2xml.specification.Specification;
 import json2xml.values.Values;
@@ -67,80 +74,132 @@ public class Json2Xml {
 		if(commandLine.hasOption("C")) {
 			int modeOfParsing = Integer.parseInt(commandLine.getOptionValue("C"));
 			if(modeOfParsing == 0) {
-				System.out.println("Parsing From XML to JSON");
+				String pathIN = commandLine.getOptionValue("FI");
+				String pathOUT = commandLine.getOptionValue("FO");
+				System.out.println("Parsing From XML: " + pathIN +  " to JSON: " + pathOUT);
+				try {
+					
+					File fXmlFile = new File(pathIN);
+					Container container = new Container();
+					UrbanDataset urban = container.getUrbanDataset();
+					
+					DocumentBuilder dBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+
+					Document doc = dBuilder.parse(fXmlFile);
+					NodeList nList = doc.getChildNodes();
+					NodeList mainTag = nList.item(1).getChildNodes();
+					int len = mainTag.getLength();
+					
+					for(int i=1;i<len; i+=2) {
+						Node node = mainTag.item(i);
+						String nodeName = node.getNodeName();
+						
+						if (nodeName.equals("specification")) {
+							
+							Specification s = container.specification(node);
+							urban.setSpecification(s);
+					       
+						}else if(nodeName.equals("context")) {
+							Context c = container.context(node);
+							urban.setContext(c);
+							
+						}else {
+							Values v = container.values(node);
+							urban.setValues(v);
+							
+						}
+						
+				        
+				        
+					}
+					Gson gson = new GsonBuilder().setPrettyPrinting().create();
+					JsonParser Jparser = new JsonParser();
+			        String jsonString = gson.toJson(container);
+			        JsonObject json = Jparser.parse(jsonString).getAsJsonObject();
+			        String prettyJson = gson.toJson(json);
+			        Writer writer = new OutputStreamWriter(new FileOutputStream(pathOUT) , "UTF-8");
+			        writer.write(prettyJson);
+					writer.close();
+					System.out.println("File json formed...");
+					
+				 } catch (Exception e) {
+					e.printStackTrace();
+			     }
+
+				}
+				
+				
 			}else {
-				System.out.println("Parsing From JSON to XML");
+				String pathIN = commandLine.getOptionValue("FI");
+				String pathOUT = commandLine.getOptionValue("FO");
+				System.out.println("Parsing From JSON: " + pathIN +  " to XML: " + pathOUT);
+				
+				try {
+					Gson gson = new Gson();
+					BufferedReader reader = new BufferedReader(new FileReader(pathIN));
+					Container result = gson.fromJson(reader, Container.class);
+					
+					
+					DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+					DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+					Document doc = docBuilder.newDocument();
+					doc.setXmlStandalone(true);
+					
+					
+					Element UD = doc.createElement("UrbanDataset");
+					Attr xmlnsxsi = doc.createAttribute("xmlns:xsi");
+					xmlnsxsi.setValue("http://www.w3.org/2001/XMLSchema-instance");
+					UD.setAttributeNode(xmlnsxsi);
+					
+					Attr schemaLocation = doc.createAttribute("xsi:schemaLocation");
+					schemaLocation.setValue("smartcityplatform:enea:information:xml:schemas:main:urbandataset http://smartcityplatform.enea.it/specification/information/1.0/xml/schemas/scps-urbandataset-schema-1.0.xsd");
+					UD.setAttributeNode(schemaLocation);
+					
+					Attr xmlnsEl = doc.createAttribute("xmlns");
+					xmlnsEl.setValue("smartcityplatform:enea:information:xml:schemas:main:urbandataset");
+					UD.setAttributeNode(xmlnsEl);
+					
+					doc.appendChild(UD);
+					Specification spec = result.getUrbanDataset().getSpecification();
+					Element specNode = spec.text(doc);
+					
+					Context cont = result.getUrbanDataset().getContext();
+					Element contextNode = cont.text(doc);
+					
+					Values val = result.getUrbanDataset().getValues();
+					Element valuesNode = val.text(doc);
+					
+					
+					UD.appendChild(specNode);
+					UD.appendChild(contextNode);
+					UD.appendChild(valuesNode);				
+					
+					TransformerFactory transformerFactory = TransformerFactory.newInstance();
+					Transformer transformer = transformerFactory.newTransformer();
+					transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+					transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+					transformer.setOutputProperty(OutputKeys.DOCTYPE_PUBLIC, "yes");
+					DOMSource source = new DOMSource(doc);
+					
+					StreamResult r = new StreamResult(new File(pathOUT));
+					transformer.transform(source, r);
+					System.out.println("File XML generated. . .");
+
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				
+				
 			}
 		}
-		
-		
-
-		
-//		String path = "PATH FILE JSON";
-//	
-//			
-//			try {
-//				Gson gson = new Gson();
-//				BufferedReader reader = new BufferedReader(new FileReader(path));
-//				Container result = gson.fromJson(reader, Container.class);
-//				
-//				String filename = "./final.xml";
-//				DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-//				DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-//				Document doc = docBuilder.newDocument();
-//				doc.setXmlStandalone(true);
-//				
-//				
-//				Element UD = doc.createElement("UrbanDataset");
-//				
-//				//// Urban Dataset attributes definition
-//				Attr xmlnsxsi = doc.createAttribute("xmlns:xsi");
-//				xmlnsxsi.setValue("http://www.w3.org/2001/XMLSchema-instance");
-//				UD.setAttributeNode(xmlnsxsi);
-//				
-//				Attr schemaLocation = doc.createAttribute("xsi:schemaLocation");
-//				schemaLocation.setValue("smartcityplatform:enea:information:xml:schemas:main:urbandataset http://smartcityplatform.enea.it/specification/information/1.0/xml/schemas/scps-urbandataset-schema-1.0.xsd");
-//				UD.setAttributeNode(schemaLocation);
-//				
-//				Attr xmlnsEl = doc.createAttribute("xmlns");
-//				xmlnsEl.setValue("smartcityplatform:enea:information:xml:schemas:main:urbandataset");
-//				UD.setAttributeNode(xmlnsEl);
-//				
-//				doc.appendChild(UD);
-//				Specification spec = result.getUrbanDataset().getSpecification();
-//				Element specNode = spec.text(doc);
-//				
-//				Context cont = result.getUrbanDataset().getContext();
-//				Element contextNode = cont.text(doc);
-//				
-//				Values val = result.getUrbanDataset().getValues();
-//				Element valuesNode = val.text(doc);
-//				
-//				
-//				UD.appendChild(specNode);
-//				UD.appendChild(contextNode);
-//				UD.appendChild(valuesNode);				
-//				
-//				TransformerFactory transformerFactory = TransformerFactory.newInstance();
-//				Transformer transformer = transformerFactory.newTransformer();
-//				transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-//				transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
-//				transformer.setOutputProperty(OutputKeys.DOCTYPE_PUBLIC, "yes");
-//				DOMSource source = new DOMSource(doc);
-//				
-//				StreamResult r = new StreamResult(new File(filename));
-//				transformer.transform(source, r);
-//				System.out.println("File XML generated. . .");
-//
-//				
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//			}
+			
+			
 			
 			
 		
 		
 
-	}
+	
 
 }
